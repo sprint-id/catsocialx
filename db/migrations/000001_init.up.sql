db6748a4feb800cbee4a6c258313aa649629b132
Muhammad Syarifudin <muhammad.el.syarifudin@gmail.com>
2024-03-30 14:57:36 +0700

    fix: update database

--- db/migrations/000001_init.up.sql
+++ db/migrations/000001_init.up.sql
@@ -28,7 +28,7 @@ CREATE TABLE IF NOT EXISTS transactions (
     balance INT,
     currency VARCHAR DEFAULT 'USD',
     transfer_proof_img VARCHAR,
-    created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
+    created_at BIGINT DEFAULT EXTRACT(EPOCH FROM NOW())
 );
 
 -- CREATE TABLE FRIENDS (
